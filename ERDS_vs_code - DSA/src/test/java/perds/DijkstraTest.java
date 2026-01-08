cf124fb6917acb504ecf562f858a1f566f1b083b
vedu <kumarvedant568@gmail.com>
2026-01-08 01:17:15 +0000

    Finish dispatch loop, predictor, and add unit tests

--- ERDS_vs_code - DSA/src/test/java/perds/DijkstraTest.java
+++ ERDS_vs_code - DSA/src/test/java/perds/DijkstraTest.java
@@ -1,11 +1,31 @@
 package perds;
 
 import org.junit.jupiter.api.Test;
+import perds.algorithm.Dijkstra;
+import perds.graph.Graph;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DijkstraTest {
+
     @Test
-    void placeholderTest() {
-        assertTrue(true);
+    void shortestPathFindsBestRoute() {
+        Graph g = new Graph();
+        g.addEdge("A", "B", 5);
+        g.addEdge("B", "C", 2);
+        g.addEdge("A", "C", 10);
+
+        Dijkstra d = new Dijkstra(g);
+        assertEquals(7.0, d.shortestPath("A", "C"));
+    }
+
+    @Test
+    void unreachableReturnsInfinity() {
+        Graph g = new Graph();
+        g.addNode("A");
+        g.addNode("B");
+
+        Dijkstra d = new Dijkstra(g);
+        assertTrue(Double.isInfinite(d.shortestPath("A", "B")));
     }
 }
